d5c0b1eda7d1161518131420d9003aee905792af
Carla Marvin <carlamarvin78@gmail.com>
2019-08-22 00:50:59 -0400

    Updated FriendsList

--- friends/src/Components/FriendsList.js
+++ friends/src/Components/FriendsList.js
@@ -2,8 +2,24 @@ import React from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 class FriendsList extends React.Component {
+    state = {friends: []
+    };
+    componentDidMount(){
+        axiosWithAuth()
+        .get('http://localhost:5000/api/friends')   
+        .then (res => {
+            console.log (res.data);
+            this.setState({friends: res.data});
+        })
+        .catch(error => console.log(error));
+    }
+
     render() {
-        return (<h1>Testing!</h1>);
+        return (this.state.friends.map(friend => <div key={friend.id}>
+            <div className="friendName">{friend.name}</div>
+            <div className="friendAge">{friend.age}</div>
+            <div className="friendEmail">{friend.email}</div>
+            </div>));
     }
 }
 
